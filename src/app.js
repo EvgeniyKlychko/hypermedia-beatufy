b3f547c52e0dbd002097d6e13824031545a9bbe1
Maksym Tatariants <m.tatariants@mobidev.biz>
2020-08-21 10:50:34 +0300

    added face beutify pipeline

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import './assets/index.css';
-import * as tf from '@tensorflow/tfjs';
+  import * as tf from '@tensorflow/tfjs';
 // import * as facemesh from './lib/facemesh';
 import * as facemesh from './lib/facemesh-custom';
 import Stats from 'stats.js';
@@ -14,8 +14,18 @@ const ctx = canvas.getContext('2d');
 //const canvas2Ctx = canvas2.getContext('2d')
 const sampling_points = [9, 18, 50, 5, 376, 187, 83, 164, 229, 299, 84, 2, 1]
 const sample_size_px = 3
-const seg_threshold = tf.tensor1d([0.075])
+const seg_threshold = tf.tensor1d([0.08])
 const box_filter_size = 5
+const brightness_boost = 0.15
+const gaussian_kernel_single_ch = tf.tensor4d([1.94254715e-07, 9.65682564e-06, 1.00626434e-04, 2.19788338e-04, 1.00626434e-04, 9.65682564e-06, 1.94254715e-07,
+                                               9.65682564e-06, 4.80061867e-04, 5.00235953e-03, 1.09261577e-02, 5.00235953e-03, 4.80061867e-04, 9.65682564e-06,
+                                               1.00626434e-04, 5.00235953e-03,  5.21257832e-02, 1.13853178e-01, 5.21257832e-02, 5.00235953e-03,1.00626434e-04,
+                                               2.19788338e-04, 1.09261577e-02, 1.13853178e-01, 2.48678204e-01, 1.13853178e-01, 1.09261577e-02, 2.19788338e-04,
+                                               1.00626434e-04, 5.00235953e-03, 5.21257832e-02, 1.13853178e-01, 5.21257832e-02, 5.00235953e-03, 1.00626434e-04,
+                                               9.65682564e-06, 4.80061867e-04, 5.00235953e-03, 1.09261577e-02, 5.00235953e-03, 4.80061867e-04, 9.65682564e-06,
+                                               1.94254715e-07, 9.65682564e-06, 1.00626434e-04, 2.19788338e-04, 1.00626434e-04, 9.65682564e-06, 1.94254715e-07],
+  [7, 7, 1, 1])
+const gaussian_kernel = tf.concat([gaussian_kernel_single_ch, gaussian_kernel_single_ch, gaussian_kernel_single_ch], 2)
 
 window.tf = tf
 
@@ -87,17 +97,12 @@ function modifyKeypoints(keypoints_orig, box, faceSize){
 
   const x = box.topLeft.slice([0], [1])
   const y = box.topLeft.slice([1], [1])
-  //console.log(x)
-  //console.log(y)
-  //console.log('orig keypoints', keypoints_orig)
+
   const keypoints_x = keypoints_orig.slice([0, 0], [-1, 1]).sub(x).mul(widthTensor)
   const keypoints_y = keypoints_orig.slice([0, 1], [-1, 1]).sub(y).mul(heightTensor)
   const keypoints_z = keypoints_orig.slice([0, 2], [-1, 1])
-  //console.log('x', keypoints_x)
-  //console.log('y', keypoints_y)
-  //console.log('z', keypoints_z)
+
   const new_coords = tf.concat([keypoints_x, keypoints_y, keypoints_z], 1)
-  //console.log('new keypoints',new_coords)
 
   return new_coords.arraySync()
 }
@@ -107,17 +112,13 @@ function sample_avg_color(img, color_coords, square_side_px){
                              [square_side_px, square_side_px])
     console.log(sample)
     return sample.mean([0, 1], true)
-    //sample = img[color_coords[0]: color_coords[0] + square_side_px,
-    //             color_coords[1]: color_coords[1] + square_side_px,
-    //             :]
-    //return sample.mean(axis=(0, 1)).reshape(1, 1, 3)
   }
 
-function boxBlur(img, kernel_size){
-  // we assume that image has shape Height, Width, Channels
-  const kernel = tf.ones([kernel_size, kernel_size, img.shape[2], img.shape[2]]).div(kernel_size*kernel_size)
+function boxBlur(mask, kernel_size){
+  // we assume that mask has shape (Height, Width, Channels)
+  const kernel = tf.ones([kernel_size, kernel_size, mask.shape[2], mask.shape[2]]).div(kernel_size*kernel_size)
   //tf.conv2d (x, filter, strides, pad, dataFormat?, dilations?, dimRoundingMode?)
-  return tf.conv2d(img, kernel, [1, 1], 'same').clipByValue(0, 1)
+  return tf.conv2d(mask, kernel, [1, 1], 'same').clipByValue(0, 1)
 }
 
 function segmentFace(face_img_tensor, keypoints, threshold, sampling_list, blur_kernel_size){
@@ -125,24 +126,42 @@ function segmentFace(face_img_tensor, keypoints, threshold, sampling_list, blur_
   //console.log('mask', mask)
   sampling_list.forEach(sample_keypoint => {
     const coords = keypoints[sample_keypoint]
-    //console.log('coords', coords)
+
     const color = sample_avg_color(face_img_tensor, coords, 5)
-    //console.log('color', color)
 
     // compute L2 norm
-    // tensor, ord='euclidean', axis=None, keepdims=None, name=None
+    // (tensor, ord='euclidean', axis=None, keepdims=None, name=None) - params
     const color_dist = tf.norm(face_img_tensor.sub(color),'euclidean', [2], true)
-    //console.log('color_dist', color_dist)
+
     const mask_sample = color_dist.less(seg_threshold).cast('int32')
     mask = mask.add(mask_sample)
 
   })
   mask = mask.clipByValue(0, 1)
   //console.log('mask final', mask)
+  //mask = tf.image.resizeBilinear(mask, [300, 300])
   mask = boxBlur(mask, box_filter_size)
-  mask = tf.image.resizeBilinear(mask, [300, 300])
-  return mask
+  face_img_tensor = face_img_tensor.mul(mask.greater(0))
+  return [face_img_tensor, mask]
 }
+
+function increase_brightnessv3(rgb_img, value){
+  const rChannel = (rgb_img.slice([0, 0, 0], [-1, -1, 1])).add((tf.tensor1d([1]).sub(rgb_img.slice([0, 0, 0], [-1, -1, 1]))).mul(value))
+  const gChannel = (rgb_img.slice([0, 0, 1], [-1, -1, 1])).add((tf.tensor1d([1]).sub(rgb_img.slice([0, 0, 1], [-1, -1, 1]))).mul(value))
+  const bChannel = (rgb_img.slice([0, 0, 2], [-1, -1, 1])).add((tf.tensor1d([1]).sub(rgb_img.slice([0, 0, 2], [-1, -1, 1]))).mul(value))
+  const new_img = tf.concat([rChannel, gChannel, bChannel], 2)
+  return new_img.clipByValue(0, 1)
+}
+
+function convole_gaussian(img, kernel){
+  return tf.depthwiseConv2d(img, kernel, [1, 1], 'same').clipByValue(0, 1)
+}
+
+function alpha_blend_simple(background_rgb, overlay_rgb, aplha_mask){
+  //return (background_rgb * (1 - aplha_mask)) + (overlay_rgb * aplha_mask) - from python
+  return (background_rgb.mul(tf.tensor1d([1]).sub(aplha_mask))).add(overlay_rgb.mul(aplha_mask))
+}
+
 
 // ---------------------- END image processing functions -------------------------
 
@@ -157,7 +176,7 @@ async function renderPrediction() {
 
   if (predictions.length > 0) {
     //const faceSmall = predictions[0].face.squeeze()
-    const faceNormal = predictions[0].face2.squeeze()
+    var faceNormal = predictions[0].face2.squeeze()
     console.log(predictions)
 
     for (const prediction of predictions){
@@ -165,10 +184,10 @@ async function renderPrediction() {
                                         prediction.boundingBox,
                                         prediction.faceSize);
 
-      const mask = segmentFace(faceNormal, keypoints, seg_threshold,
-                               sampling_points, box_filter_size)
+      var [seg_skin, mask] = segmentFace(faceNormal, keypoints, seg_threshold,
+                                           sampling_points, box_filter_size)
 
-      // commented old keypoints render code
+      // commented keypoints render code
       //for (let i = 0; i < keypoints.length; i++) {
         //const x = keypoints[i][0];
         //const y = keypoints[i][1];
@@ -178,12 +197,18 @@ async function renderPrediction() {
         //ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
         //ctx.fill();
         //}
-        await tf.browser.toPixels(mask, canvas)
+        //whitening
+        seg_skin = increase_brightnessv3(seg_skin, brightness_boost)
+        // skin smoothing
+        seg_skin = convole_gaussian(seg_skin, gaussian_kernel)
+        // adding to the original image
+        faceNormal = alpha_blend_simple(faceNormal, seg_skin, mask)
+        await tf.browser.toPixels(faceNormal, canvas)
         mask.dispose()
       }
 
       img.dispose()
-    //faceSmall.dispose()
+      //faceSmall.dispose()
       faceNormal.dispose()
 
 }
