a58000831d1f66394524c538667e2eeded0b0925
evgeniy.klychko <evgeniy.klychko@gmail.com>
2020-11-26 17:22:35 +0200

    update default preset

--- demo-app/src/PresetManager.js
+++ demo-app/src/PresetManager.js
@@ -3,60 +3,19 @@ import { Preset } from './Lib';
 export class PresetManager {
 
   constructor(gui) {
+    console.log('PresetManager', this);
     this.guiFolder = gui.addFolder('Presets');
     this.guiFolder.open();
 
     this.presetList = [
-      new Preset(this.guiFolder, `Blur(Scale 0.1, Radius 3px)`, (preset) => this.applyPreset(preset), {
-        MSIZE: 3,
-        SKIN_DETECTION: true,
-        Brightness: 1,
-        SIGMA: 15,
-        BSIGMA: 0.1
-      }),
-      new Preset(this.guiFolder, `Blur(Scale 0.1, Radius 7px)`, (preset) => this.applyPreset(preset), {
-        MSIZE: 7,
-        SKIN_DETECTION: true,
-        Brightness: 1,
-        SIGMA: 15,
-        BSIGMA: 0.1
-      }),
-      new Preset(this.guiFolder, `Blur(Scale 0.1, Radius 15px)`, (preset) => this.applyPreset(preset), {
-        MSIZE: 15,
-        SKIN_DETECTION: true,
-        Brightness: 1,
-        SIGMA: 15,
-        BSIGMA: 0.1
-      }),
-      new Preset(this.guiFolder, `Blur(Scale 0.1, Radius 25px)`, (preset) => this.applyPreset(preset), {
-        MSIZE: 25,
-        SKIN_DETECTION: true,
-        Brightness: 1,
-        SIGMA: 15,
-        BSIGMA: 0.1
-      }),
-      new Preset(this.guiFolder, `Blur(Scale 0.1, Radius 31px)`, (preset) => this.applyPreset(preset), {
-        MSIZE: 31,
-        SKIN_DETECTION: true,
-        Brightness: 1,
-        SIGMA: 15,
-        BSIGMA: 0.1
-      }),
-      new Preset(this.guiFolder, `Blur(Scale 1, Radius 15px)`, (preset) => this.applyPreset(preset), {
-        MSIZE: 15,
-        SKIN_DETECTION: true,
-        Brightness: 1,
-        SIGMA: 15,
-        BSIGMA: 1
-      }),
-      new Preset(this.guiFolder, `Blur(Scale 10, Radius 15px)`, (preset) => this.applyPreset(preset), {
-        MSIZE: 15,
-        SKIN_DETECTION: true,
-        Brightness: 1,
-        SIGMA: 15,
-        BSIGMA: 10
-      })
+      new Preset(`Blur(Radius 15px)`)
     ];
+
+    this.presetList.forEach(p => {
+      const folder = this.guiFolder.addFolder(`Preset ${p.name}`);
+      folder.open();
+      folder.add(p.defines, 'MSIZE', 3, 31, 1).name(`Radius`);
+    });
   }
 
   setRenderPass(pass) {
